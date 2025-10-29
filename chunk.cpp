99667d37871e61810e0bbdeaac4842209ca821d6
watari1337 <alexboyip@gmail.com>
2025-10-29 08:48:24 +0300

    add perling 2d
    add generation of cave by perling noise 2d

--- chunk.cpp
+++ chunk.cpp
@@ -15,16 +15,37 @@ void Chunk::generate()
     paint.setPen(Qt::NoPen);
     paint.fillRect(0, 0, CHUNKSIZE, CHUNKSIZE, m_sky);
     //ground line
-    for (int i = 0; i < m_groundLine.size(); i++) {
-        int globalX = m_chunkX * CHUNKSIZE + i;
-        int globalY = (500 + 300 * noisePerlin(globalX/9));
-        int localY = globalY - (m_chunkY * CHUNKSIZE);
-        m_groundLine[i] = localY;
+    int globalYBottom = (m_chunkY+1) * CHUNKSIZE;
+    int globalX = m_chunkX * CHUNKSIZE;
+    for (int i = 0; i < CHUNKSIZE; i++) {
+        int globalLandY = (LANDSTART + LANDDEVIATION * noisePerlin(globalX/9));
+        int localY = globalLandY - (m_chunkY * CHUNKSIZE);
+        m_groundLine[i] = globalLandY;
         //draw ground line
         if (localY >= 0 && localY < Chunk::CHUNKSIZE){
             paint.fillRect(i, localY, 1, Chunk::CHUNKSIZE - localY, m_grass);
-        } else if (localY < 0){
+        } else if (localY < 0 && globalYBottom <= STONEPOS){
             paint.fillRect(i, 0, 1, Chunk::CHUNKSIZE - localY, m_grass);
+        } else if (globalYBottom > STONEPOS){
+            paint.fillRect(0, 0, CHUNKSIZE, CHUNKSIZE, m_stone);
+        }
+        globalX++;
+    }
+    //cave
+    QPen stPen;
+    stPen.setWidth(0);
+    stPen.setColor(m_caveAir);
+    paint.setPen(stPen);
+    for (int i = 0; i < CHUNKSIZE; ++i) {
+        int globalX = m_chunkX * CHUNKSIZE + i;
+        for (int j = 0; j < CHUNKSIZE; ++j) {
+            int globalY = m_chunkY * CHUNKSIZE + j;
+            if (STONEPOS < globalY){
+                float z = noisePerlin2D(globalX, globalY);
+                if (z > 0.1f){
+                    paint.drawPoint(i, j);
+                }
+            }
         }
     }
     //draw border
@@ -62,21 +83,64 @@ QVector<int> Chunk::getGroundLine() const
     return m_groundLine;
 }
 
-float Chunk::getNoiseAngle(int x, int y)
-{
-    uint32_t X = static_cast<uint32_t>(x);
-    uint32_t Y = static_cast<uint32_t>(y);
-    uint32_t h = X * 374761393 + Y * 845143023 + SEED * 73856093;
-    h = (h ^ (h >> 13)) * 1274126177;
-    return (h & 0xFFFFFF) / float(0xFFFFFF) * 2.0f - 1.0f; // значение 0..1
-}
-
 float fade(float t){
     return  t*t*t * (10 + t*(-15 + 6*t));
 }
 
 float interpol(float start, float end, float pos){
     return start + fade(pos)*(end-start);
+}
+
+float Chunk::getNoiseAngle(int x)
+{
+    uint32_t X = static_cast<uint32_t>(x);
+    uint32_t h = X * 374761393u + SEED * 73856093u;
+    h = (h ^ (h >> 13)) * 1274126177u;
+    return (h & 0xFFFFFF) / float(0xFFFFFF) * 2.0f - 1.0f; // значение -1..1
+}
+
+float Chunk::getHashAngle(int x, int y)
+{
+    uint32_t X = static_cast<uint32_t>(x);
+    uint32_t Y = static_cast<uint32_t>(y);
+    uint32_t h = X * 516845816u + Y * 879588622u + SEED * 768459135u;
+    h = (h ^ (h >> 13)) * 7458948215u;
+    return (h & 0xFFFFFF) / float(0xFFFFFF); // значение 0..1
+}
+
+QVector2D Chunk::getPerlinVector2D(int x, int y)
+{
+    float angle = getHashAngle(x, y);
+    angle *= 2.0f * M_PI;
+    return QVector2D(cos(angle), sin(angle));
+}
+
+float Chunk::noisePerlin2D(int globalX, int globalY)
+{
+    int vectorX = std::floor(float(globalX) / VECTORCELLSIZE2D);
+    int vectorY = std::floor(float(globalY) / VECTORCELLSIZE2D);
+
+    float dx = float(globalX - vectorX * VECTORCELLSIZE2D) / VECTORCELLSIZE2D;
+    float dy = float(globalY - vectorY * VECTORCELLSIZE2D) / VECTORCELLSIZE2D;
+
+    QVector2D v00 = getPerlinVector2D(vectorX, vectorY);
+    QVector2D v01 = getPerlinVector2D(vectorX, vectorY+1);
+    QVector2D v10 = getPerlinVector2D(vectorX+1, vectorY);
+    QVector2D v11 = getPerlinVector2D(vectorX+1, vectorY+1);
+
+    float dot00 = QVector2D::dotProduct(v00, QVector2D(dx, dy));
+    float dot01 = QVector2D::dotProduct(v01, QVector2D(dx, dy-1));
+    float dot10 = QVector2D::dotProduct(v10, QVector2D(dx-1, dy));
+    float dot11 = QVector2D::dotProduct(v11, QVector2D(dx-1, dy-1));
+
+    float fadeX = fade(dx);
+    float fadeY = fade(dy);
+
+    float pol1 = interpol(dot00, dot10, fadeX);
+    float pol2 = interpol(dot01, dot11, fadeX);
+    float pol3 = interpol(pol1, pol2, fadeY);
+
+    return pol3;
 }
 
 float Chunk::noisePerlin(float x)
@@ -89,3 +153,4 @@ float Chunk::noisePerlin(float x)
     float d1 = v1 * (dx0 - 1.0f);
     return (interpol(d0, d1, dx0));
 }
+
--- chunk.h
+++ chunk.h
@@ -5,6 +5,7 @@
 #include <QHash>
 #include <QPixmap>
 #include <QPainter>
+#include <QPen>
 #include <QColor>
 #include <QVector2D>
 
@@ -20,24 +21,32 @@ public:
     QVector<int> getGroundLine() const;
 
     static const int CHUNKSIZE = 128; //width and heigh of chunk
-    static const int VECTORCELLSIZE = 50; //size of vector matrix
+    static const int VECTORCELLSIZE = 50; //size of vector line
+    static const int VECTORCELLSIZE2D = 200; //size of vector matrix
     static const int VECTORFORGET = 100; //If we are farther than this num of vector forgot it
     static const int TOPCHUNKBORDER = -40; //через сколько чанков top будет граница
     static const int BOTTOMCHUNKBORDER = 40; //через сколько чанков bottom будет граница
-    static const int LEFTCHUNKBORDER = -40; //через сколько чанков left будет граница
-    static const int RIGHTCHUNKBORDER = 40; //через сколько чанков right будет граница
+    static const int LEFTCHUNKBORDER = -150; //через сколько чанков left будет граница
+    static const int RIGHTCHUNKBORDER = 150; //через сколько чанков right будет граница
     static const int WIDTHBORDER = 10; //через сколько чанков right будет граница
+    static const int LANDSTART = 500; //через сколько start medium line of land
+    static const int LANDDEVIATION = 300; //maximum deviation of land line
+    static const int STONESTART = 400; //через сколько after medium land line start line of stone
+    static const int STONEPOS = LANDSTART + STONESTART; //через сколько after medium land line start line of stone
 
 private:
 
-    QColor m_grass = QColor(50, 201, 65), m_sky = QColor(111, 183, 209), m_border = QColor(123, 143, 194);
+    QColor m_grass = QColor(50, 201, 65), m_sky = QColor(111, 183, 209), m_border = QColor(123, 143, 194),
+        m_stone = QColor(112, 125, 117), m_caveAir = QColor(79, 115, 100);
 
     int m_chunkX, m_chunkY;
     QPixmap m_pixmap = QPixmap(CHUNKSIZE, CHUNKSIZE);
     QVector<int> m_groundLine = QVector<int>(CHUNKSIZE);
-    float getNoiseAngle(int x, int y = 0);
+    float getNoiseAngle(int x);
     float noisePerlin(float x);
-    void noiseSimplex();
+    float getHashAngle(int x, int y);
+    QVector2D getPerlinVector2D(int x, int y);
+    float noisePerlin2D(int globalX, int globalY); //значение по z от 0 до 1
 };
 
 #endif // CHUNK_H
